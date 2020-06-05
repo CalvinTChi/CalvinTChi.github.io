d7ae1db31cf33e3a3b33332f7b3703ab20321b4d
Calvin Chi <calvinchi@Calvins-MacBook-Pro-3.local>
2020-06-05 01:03:29 -0700

    update slides

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # update master branc
-mkdir -p /tmp/workspaceh
+mkdir -p /tmp/workspace
+cp -r * /tmp/workspace/
 git add -A .
 git commit -m "update slides"
 
@@ -15,3 +16,4 @@ git push origin master gh-pages
 # push update
 git checkout master
 rm -rf /tmp/workspace
+
